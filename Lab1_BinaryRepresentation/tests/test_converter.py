d2d9907aea010443b77837544dd1cbbb6423bc75
shkibidilnick <feechkawinks1987@gmail.com>
2026-03-20 22:36:17 +0300

    refactor: Move integer logic to integer package

--- Lab1_BinaryRepresentation/tests/test_converter.py
+++ Lab1_BinaryRepresentation/tests/test_converter.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from converter import to_direct_code
+from integer.converter import to_direct_code
 from constants import BIT_SIZE, POSITIVE_SIGN, NEGATIVE_SIGN, MAX_VALUE
 
 
